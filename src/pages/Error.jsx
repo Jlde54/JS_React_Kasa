3f905f7aa320bead40d69492aed68d297fa244e4
Jlde54 <jlde54@sfr.fr>
2025-01-24 10:09:14 +0100

    transfered images and logos to src/assets/

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -6,7 +6,7 @@ function Error() {
     return (
         <div className={styles.error}>
             <p className={styles.error__404}>404</p>
-            <p className={styles.error__msg}>Oups! La page que vous demandez n'existe pas</p>
+            <p className={styles.error__msg}>Oups! La page que vous demandez n&apos;existe pas</p>
             <nav className={styles.error__nav}>
                 <Link className={styles.error__link} to={'/'}>Retourner sur la page d&apos;accueil</Link>
             </nav>
